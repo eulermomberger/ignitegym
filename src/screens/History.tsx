23e8489f2c79b8732a4810d65a6f3e17ad0d2ac0
Euler <mombergereuler@gmail.com>
2024-01-20 14:29:53 -0300

    feat: Finalizando as interfaces

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,51 @@
-import { Center, Text } from 'native-base';
+import { useState } from 'react';
+import { Heading, SectionList, Text, VStack } from 'native-base';
+
+import { HistoryCard } from '@components/HistoryCard';
+import { ScreenHeader } from '@components/ScreenHeader';
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.08.22',
+      data: ['Puxada frontal', 'Remada unilateral'],
+    },
+    {
+      title: '27.08.22',
+      data: ['Puxada frontal'],
+    },
+  ]);
+
   return (
-    <Center flex={1}>
-      <Text color='white'>History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title='Histórico de exercícios'/>
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderSectionHeader={({ section }) => (
+          <Heading
+            color='gray.200'
+            fontSize='md'
+            mt={10}
+            mb={3}
+          >
+            {section.title}
+          </Heading>
+        )}
+        renderItem={({ item }) => (
+          <HistoryCard/>
+        )}
+        ListEmptyComponent={() => (
+          <Text color='gray.100' textAlign='center'>
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
+        contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+        showsVerticalScrollIndicator={false}
+        px={8}
+      />
+    </VStack>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,69 @@
-import { Center, Text } from 'native-base';
+import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { FlatList, HStack, Heading, Text, VStack } from 'native-base';
+
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
+
+import { ExerciseCard } from '@components/ExerciseCard';
+import { Group } from '@components/Group';
+import { HomeHeader } from '@components/HomeHeader';
 
 export function Home() {
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro']);
+  const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curva', 'Remada unilateral', 'Levantamento terra']);
+  const [groupSelected, setGroupSelected] = useState('Costas');
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  const handleOpenExerciseDetails = () => {
+    navigation.navigate('exercise');
+  };
+
   return (
-    <Center flex={1}>
-      <Text color='white'>Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <HomeHeader/>
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+        minH={10}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent='space-between' mb={5}>
+          <Heading color='gray.200' fontSize='md'>
+            Exercícios
+          </Heading>
+
+          <Text color='gray.200' fontSize='sm'>
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+          renderItem={({ item }) => (
+            <ExerciseCard
+              onPress={handleOpenExerciseDetails}
+            />
+          )}
+        />
+      </VStack>
+    </VStack>
   );
 }
