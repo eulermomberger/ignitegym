c236d228b496744e212a50a2106aa0558f4f669a
Euler <mombergereuler@gmail.com>
2024-01-31 23:03:46 -0300

    feat: Implementar a possibilidade de alterar a foto do perfil

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from 'native-base';
+import { Center, Heading, ScrollView, Skeleton, Text, VStack, useToast } from 'native-base';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
@@ -11,6 +13,41 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('https://github.com/eulermomberger.png');
+  const toast = useToast();
+
+  const handleUserPhotoSelect = async () => {
+    setPhotoIsLoading(true);
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) return;
+
+      if (photoSelected.assets[0]?.uri) {
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0]?.uri);
+
+        if (photoInfo.exists && ((photoInfo.size / 1024 / 1024) > 5)) {
+          toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+            placement: 'top',
+            bgColor: 'red.500',
+          });
+          return;
+        }
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  };
 
   return (
     <VStack flex={1}>
@@ -29,13 +66,13 @@ export function Profile() {
               />
             :
               <UserPhoto
-                source={{ uri: 'https://github.com/eulermomberger.png' }}
+                source={{ uri: userPhoto }}
                 alt='Foto do usuário'
                 size={PHOTO_SIZE}
               />
           }
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text color='green.500' fontWeight='bold' fontSize='md' mt={2} mb={8}>
               Alterar foto
             </Text>
